b6591e8d775979c5c3e6843522f201f91931c1c4
Timo <jootimo@gmail.com>
2018-02-20 10:40:38 +0200

    Small fixes and cleanup

--- non_signal_data.py
+++ non_signal_data.py
@@ -141,14 +141,14 @@ plt.savefig("AccuracyHist.png")
 
 def select_features(features, labels, num_selections):
     '''
-    Get a feature matrix with a desired number of the features that correlate most to the labels.
+    Get indices of the columns of a desired number of features that correlate most to the labels.
     Kendall tau coefficient is used.
 
     @param  features        2D list of the feature values where to select
-    @param  labels          List of the corresponding labels          
+    @param  labels          List of the corresponding labels
     @param  num_selections  How many features to select
 
-    @return 2D list of feature values
+    @return List of column indices
     '''
 
     #List of correlation-column index pairs
@@ -168,16 +168,13 @@ def select_features(features, labels, num_selections):
     selections = correlations[0 : num_selections]
 
     #Select only the requested amount of columns
-    selected_features = []
+    selected_indices = []
     for ix_row in range(len(features)):
         row = []
         for sel in selections:
-            ix_col = sel[1]
-            row.append(features[ix_row][ix_col])
+            selected_indices.append(sel[1])
         
-        selected_features.append(row)
-
-    return(selected_features)
+    return(selected_indices)
 
 def loo_cv_with_feat_selection (features, labels, num_features, f_predict):
     '''
@@ -194,13 +191,25 @@ def loo_cv_with_feat_selection (features, labels, num_features, f_predict):
     predictions = []
     correct_predictions = 0
     for test_ix, test_features in enumerate(features):
-        test_features_sel = select_features([test_features], [labels[test_ix]], num_features)
     
         training_features = [row for i, row in enumerate(features) if i != test_ix]
         training_labels = [row for i, row in enumerate(labels) if i != test_ix]
-        training_features_sel = select_features(training_features, training_labels, num_features)
+        selected_feature_indices = select_features(training_features, training_labels, num_features)
 
-        prediction = f_predict(test_features_sel, training_features_sel, training_labels)
+        #Create test and training lists that only contain the selected columns
+        training_features_sel = []
+        test_features_sel = []
+        for feat_row in training_features:
+            row = []
+            for ix in selected_feature_indices:
+                row.append(feat_row[ix])
+            training_features_sel.append(row)
+
+        for ix in selected_feature_indices:
+            test_features_sel.append(test_features[ix])
+
+        #Predict label        
+        prediction = f_predict([test_features_sel], training_features_sel, training_labels)
         predictions.append(prediction)
         
         #See if we guessed correctly
@@ -235,8 +244,15 @@ for i in range(100):
     accuracies_right.append(loo_cv_with_feat_selection(X, Y, 10, f_predict))
 
     # The wrong way to do feature selection: as a preprocessing step.
-    X_selected = select_features(X, Y, 10) 
-    accuracies_wrong.append(loo_cv(X_selected, Y, f_predict))
+    X_selected_indices = select_features(X, Y, 10)
+    X_selected_features = []
+    for feat_row in X:
+        row = []
+        for ix in X_selected_indices:
+            row.append(feat_row[ix])
+        X_selected_features.append(row)
+
+    accuracies_wrong.append(loo_cv(X_selected_features, Y, f_predict))
 
 
 print("Mean accuracy of the wrong way: " + str(np.mean(accuracies_wrong)))
@@ -251,98 +267,3 @@ plt.title("Prediction accuracies with feature selections done wrong and right")
 plt.xlabel("Prediction accuracy")
 plt.ylabel("Frequency")
 plt.savefig("FeatureSelection.png")
-
-'''
-
-Rows: 20
-Accuracy mean: 0.4425
-Accuracy variance: 0.02311875
-Percentage of cross-validation runs with over 60% accuracy: 10.0
-Percentage of cross-validation runs with over 70% accuracy: 5.0
-
-Rows: 50
-Accuracy mean: 0.49420000000000003
-Accuracy variance: 0.009058360000000001
-Percentage of cross-validation runs with over 60% accuracy: 12.0
-Percentage of cross-validation runs with over 70% accuracy: 1.0
-
-Rows: 100
-Accuracy mean: 0.48869999999999997
-Accuracy variance: 0.00508131
-Percentage of cross-validation runs with over 60% accuracy: 7.000000000000001
-Percentage of cross-validation runs with over 70% accuracy: 0.0
-
-Rows: 500
-Accuracy mean: 0.49838
-Accuracy variance: 0.0010084556000000003
-Percentage of cross-validation runs with over 60% accuracy: 0.0
-Percentage of cross-validation runs with over 70% accuracy: 0.0
-Mean accuracy of the wrong way: 0.7463333333333333
-Mean accuracy of the right way: 0.47733333333333333
-
-
-
-Rows: 20
-Accuracy mean: 0.46649999999999997
-Accuracy variance: 0.022902750000000003
-Percentage of cross-validation runs with over 60% accuracy: 14.000000000000002
-Percentage of cross-validation runs with over 70% accuracy: 2.0
-
-Rows: 50
-Accuracy mean: 0.4854
-Accuracy variance: 0.010326840000000002
-Percentage of cross-validation runs with over 60% accuracy: 14.000000000000002
-Percentage of cross-validation runs with over 70% accuracy: 1.0
-
-Rows: 100
-Accuracy mean: 0.5054000000000001
-Accuracy variance: 0.00440484
-Percentage of cross-validation runs with over 60% accuracy: 8.0
-Percentage of cross-validation runs with over 70% accuracy: 0.0
-
-Rows: 500
-Accuracy mean: 0.49648
-Accuracy variance: 0.0010772896000000006
-Percentage of cross-validation runs with over 60% accuracy: 0.0
-Percentage of cross-validation runs with over 70% accuracy: 0.0
-
-Mean accuracy of the wrong way: 0.7476666666666668
-Mean accuracy of the right way: 0.4676666666666667
-'''
-
-
-'''
-accuracies_flattened = []
-for ix, accuracy_list in enumerate(accuracies):
-    for ix_acc, acc in enumerate(accuracy_list):
-        accuracies_flattened.append(acc)
-
-print("\nOverall mean accuracy: " + str(np.mean(accuracies_flattened)))
-
-# How large fraction (in %) of the leave-one-out runs resulted in performance higher than 0.60? What about 0.70?
-num_over_06 = 0
-num_over_07 = 0
-num_accuracies = len(accuracies_flattened)
-for acc in accuracies_flattened:
-    if acc > 0.6:
-        num_over_06 += 1
-        if acc > 0.7:
-            num_over_07 += 1
-
-percent_over_06 = (num_over_06 / num_accuracies) * 100
-percent_over_07 = (num_over_07 / num_accuracies) * 100
-
-print("Overall percentage of cross-validation runs with over 60% accuracy: " + str(percent_over_06))
-print("Overall percentage of cross-validation runs with over 70% accuracy: " + str(percent_over_07))
-
-#Plot a histogram
-plt.hist(accuracies_flattened, 20)
-plt.title("Accuracies from " + str(num_accuracies) + " cross-validation runs")
-plt.xlabel("Prediction accuracy")
-plt.ylabel("Frequency")
-plt.savefig("AccuracyHist.png")
-
-#fig = plt.gcf()
-
-#plot_url = py.plot_mpl(fig, filename='Accuracy_hist')
-'''
